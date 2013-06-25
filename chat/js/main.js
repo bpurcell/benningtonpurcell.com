e36d4b64e8fe585138eb67b00ce2047bebff4fbf
bennington purcell <bennington.purcell@gmail.com>
2013-06-25 13:08:18 -0400

    fix scroll on window load

--- chat/js/main.js
+++ chat/js/main.js
@@ -8,53 +8,39 @@ var currentStatus = "★";
 
 var userListRef = new Firebase("https://purcellchat.firebaseIO.com/userlist");
 
-// Generate a reference to a new location for my user with push.
 var myUserRef = userListRef.push();
 
-// Get a reference to my own presence status.
 var connectedRef = new Firebase("https://purcellchat.firebaseIO.com/.info/connected");
 connectedRef.on("value", function(isOnline) {
   if (isOnline.val()) {
-    // If we lose our internet connection, we want ourselves removed from the list.
     myUserRef.onDisconnect().remove();
-
-    // Set our initial online status.
     setUserStatus("★");
   } else {
 
-    // We need to catch anytime we are marked as offline and then set the correct status. We
-    // could be marked as offline 1) on page load or 2) when we lose our internet connection
-    // temporarily.
     setUserStatus(currentStatus);
   }
 });
 
-// A helper function to let us set our own state.
 function setUserStatus(status) {
-  // Set our status in the list of online users.
   currentStatus = status;
   myUserRef.set({ name: name, status: status });
 }
 
-// Update our GUI to show someone"s online status.
 userListRef.on("child_added", function(snapshot) {
   var user = snapshot.val();
   $("#presenceDiv").append($("<div/>").attr("id", snapshot.name()));
   $("#" + snapshot.name()).text(user.name + "  " + user.status);
 });
 
-// Update our GUI to remove the status of a user who has left.
 userListRef.on("child_removed", function(snapshot) {
   $("#" + snapshot.name()).remove();
 });
 
-// Update our GUI to change a user"s status.
 userListRef.on("child_changed", function(snapshot) {
   var user = snapshot.val();
   $("#" + snapshot.name()).text(user.name + " " + user.status);
 });
 
-// Use idle/away/back events created by idle.js to update our status information.
 document.onIdle = function () {
   setUserStatus("☆");
 }
@@ -99,6 +85,8 @@ setAwayTimeout(60000);
       
     $('#messageWrap').scrollTop($('#messageWrap')[0].scrollHeight);
   });
+    
+  $('#messageWrap').scrollTop($('#messageWrap')[0].scrollHeight);
   
   function linkify(inputText) {
       var replacedText, replacePattern1, replacePattern2, replacePattern3;
